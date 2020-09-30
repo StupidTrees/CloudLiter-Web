4ca714e179be045024671e99cd4ea67d53a7a3df
StupidTrees <2321182768@qq.com>
2020-09-30 13:55:55 +0800

    Service层的异步函数内部统一改用同步写法

--- service/relationService.js
+++ service/relationService.js
@@ -12,28 +12,32 @@ const codes = require('../utils/codes').codes
  * @returns {Promise<{code: *, data: null, message: *}|{code: *, message: *}>}
  */
 exports.getFriends = async function (id) {
-    return await repository.getFriendsWithId(id).then((value) => {
-        let res = []
-        value.forEach(function (item, index) {
-            let usr = item.get().user.get()
-            res.push({
-                group: item.get().group,
-                user: {
-                    username:usr.username,
-                    nickname:usr.nickname,
-                    id:usr.id,
-                    gender:usr.gender,
-                    avatar:usr.avatar
-                }
-            })
-        })
-        console.log("result", res)
-        return Promise.resolve(jsonUtils.getResponseBody(codes.success,res));
-
-    }, (err) => {
-        console.log('err', err)
+    let value = null
+    try{
+        value = await repository.getFriendsWithId(id)
+    }catch (e){
         return Promise.reject(jsonUtils.getResponseBody(codes.other_error,err))
+    }
+    if(value==null){
+        return Promise.reject(jsonUtils.getResponseBody(codes.other_error))
+    }
+    //构建结果列表
+    let res = []
+    value.forEach(function (item, index) {
+        let usr = item.get().user.get()
+        res.push({
+            group: item.get().group,
+            user: {
+                username:usr.username,
+                nickname:usr.nickname,
+                id:usr.id,
+                gender:usr.gender,
+                avatar:usr.avatar
+            }
+        })
     })
+    console.log("result", res)
+    return Promise.resolve(jsonUtils.getResponseBody(codes.success,res));
 }
 
 
@@ -44,24 +48,30 @@ exports.getFriends = async function (id) {
  * @returns {Promise<{code: *, data: null, message: *}|{code: *, message: *}>}
  */
 exports.makeFriends = async function(user1,user2){
-    // noinspection EqualityComparisonWithCoercionJS
-    if(user1==user2){
+    //不能和自己成为好友
+    if(user1.trim()===user2.trim()){
         return Promise.reject(jsonUtils.getResponseBody(codes.make_friends_with_myself))
     }
-    return await repository.makeFriends(user1,user2).then((value)=>{
+    try{
+        await repository.makeFriends(user1,user2)
         return Promise.resolve(jsonUtils.getResponseBody(codes.success))
-    }).catch((err)=>{
-        if(err.original.code==='ER_DUP_ENTRY'){
+    }catch (err){
+        if(err.original.code==='ER_DUP_ENTRY'){ //主键重复，即已经是好友
             return Promise.reject(jsonUtils.getResponseBody(codes.already_friends))
-        }else if(err.original.code==='ER_NO_REFERENCED_ROW_2'){
+        }else if(err.original.code==='ER_NO_REFERENCED_ROW_2'){ //外键不存在，即有一个用户id是假的
             return Promise.reject(jsonUtils.getResponseBody(codes.make_friends_with_ghost))
         }
         return Promise.reject(jsonUtils.getResponseBody(codes.other_error,err))
-    })
+    }
 }
 
 
-
+/**
+ * 判断两用户是否是好友
+ * @param user1
+ * @param user2
+ * @returns {Promise<{code: *, data: null, message: *}|{code: *, message: *}>}
+ */
 exports.isFriend = async function(user1,user2){
     return await repository.isFriend(user1,user2).then((count)=>{
         return Promise.resolve(jsonUtils.getResponseBody(codes.success,count>0))
--- service/userService.js
+++ service/userService.js
@@ -33,28 +33,51 @@ exports.userSignUp = async function (username, password, gender, nickname) {
         return jsonUtils.getResponseBody(codes.format_error_gender)
     }
 
-
-    //数据库操作：新建用户
-    return await repository.createUser(username, password, gender, nickname).then((value) => {
-        console.log('value', value)
-        let token = tokenUtils.signIdToken(value.id, username)
-        return Promise.resolve(jsonUtils.getResponseBody(codes.success, {
-            token: token,
-            info: {
-                username: username,
-                id: value.id,
-                nickname: nickname,
-                gender: gender
-            }
-        }))
-    }, (err) => {
-        console.log('err', err)
-        if (err.original.code === 'ER_DUP_ENTRY') {
+    //通知数据库创建新用户
+    let value = null
+    try {
+        value = await repository.createUser(username, password, gender, nickname)
+    } catch (e) {
+        if (e.original.code === 'ER_DUP_ENTRY') { //有重复主键，说明用户已存在
             return Promise.reject(jsonUtils.getResponseBody(codes.signup_duplicated_username))
         } else {
-            return Promise.reject(jsonUtils.getResponseBody(code.signup_other_error))
+            return Promise.reject(jsonUtils.getResponseBody(code.signup_other_error, e))
         }
-    })
+    }
+    //若数据库插入成功，value将会是创建的新用户
+    console.log('value', value)
+    let token = tokenUtils.signIdToken(value.id, username) //为之签发token
+    return Promise.resolve(jsonUtils.getResponseBody(codes.success, {
+        token: token,
+        info: { //将用户信息也顺便返回
+            username: username,
+            id: value.id,
+            nickname: nickname,
+            gender: gender
+        }
+    }))
+
+    // //数据库操作：新建用户
+    // return await repository.createUser(username, password, gender, nickname).then((value) => {
+    //     console.log('value', value)
+    //     let token = tokenUtils.signIdToken(value.id, username)
+    //     return Promise.resolve(jsonUtils.getResponseBody(codes.success, {
+    //         token: token,
+    //         info: {
+    //             username: username,
+    //             id: value.id,
+    //             nickname: nickname,
+    //             gender: gender
+    //         }
+    //     }))
+    // }, (err) => {
+    //     console.log('err', err)
+    //     if (err.original.code === 'ER_DUP_ENTRY') {
+    //         return Promise.reject(jsonUtils.getResponseBody(codes.signup_duplicated_username))
+    //     } else {
+    //         return Promise.reject(jsonUtils.getResponseBody(code.signup_other_error))
+    //     }
+    // })
 }
 
 /***
@@ -64,38 +87,70 @@ exports.userSignUp = async function (username, password, gender, nickname) {
  * @returns {Promise<{code: *, message: *} | {code: *, message: *}>}
  */
 exports.userLogin = async function (username, password) {
-    return await repository.getUserByUsername(username).then(
-        (value) => {
-            if (value.length === 0) { //获取到的用户数量为0：用户不存在
-                return Promise.reject(jsonUtils.getResponseBody(codes.login_wrong_username, null))
-            }
-            const user = value[0].get()
-            //验证密码
-            if (user.password === password) {
-                console.log('login', '密码正确')
-                //密码正确，签发token
-                let token = tokenUtils.signToken(user)
-                return Promise.resolve(jsonUtils.getResponseBody(codes.success,
-                    {
-                        info: {
-                            id: user.id,
-                            username: user.username,
-                            nickname: user.nickname,
-                            gender: user.gender,
-                            avatar: user.avatar
-                        },
-                        token: token
-                    }))
-            } else {
-                console.log('login', '密码错误')
-                return Promise.reject(jsonUtils.getResponseBody(codes.login_wrong_password))
-            }
-        }, (err) => {
-            console.log('error', err)
-            return Promise.reject(jsonUtils.getResponseBody(
-                codes.other_error, err
-            ))
-        })
+    //根据username，从数据库中读取用户
+    let value = null
+    try {
+        value = await repository.getUserByUsername(username)
+    } catch (e) {
+        return Promise.reject(jsonUtils.getResponseBody(codes.other_error, e))
+    }
+    if (value.length === 0) { //获取到的用户数量为0：用户不存在
+        return Promise.reject(jsonUtils.getResponseBody(codes.login_wrong_username, null))
+    }
+    const user = value[0].get()
+    //验证该用户的密码
+    if (user.password === password) {
+        console.log('login', '密码正确')
+        //密码正确，签发token
+        let token = tokenUtils.signToken(user)
+        return Promise.resolve(jsonUtils.getResponseBody(codes.success,
+            {
+                info: {
+                    id: user.id,
+                    username: user.username,
+                    nickname: user.nickname,
+                    gender: user.gender,
+                    avatar: user.avatar
+                },
+                token: token
+            }))
+    } else {
+        console.log('login', '密码错误')
+        return Promise.reject(jsonUtils.getResponseBody(codes.login_wrong_password))
+    }
+
+    // return await repository.getUserByUsername(username).then(
+    //     (value) => {
+    //         if (value.length === 0) { //获取到的用户数量为0：用户不存在
+    //             return Promise.reject(jsonUtils.getResponseBody(codes.login_wrong_username, null))
+    //         }
+    //         const user = value[0].get()
+    //         //验证密码
+    //         if (user.password === password) {
+    //             console.log('login', '密码正确')
+    //             //密码正确，签发token
+    //             let token = tokenUtils.signToken(user)
+    //             return Promise.resolve(jsonUtils.getResponseBody(codes.success,
+    //                 {
+    //                     info: {
+    //                         id: user.id,
+    //                         username: user.username,
+    //                         nickname: user.nickname,
+    //                         gender: user.gender,
+    //                         avatar: user.avatar
+    //                     },
+    //                     token: token
+    //                 }))
+    //         } else {
+    //             console.log('login', '密码错误')
+    //             return Promise.reject(jsonUtils.getResponseBody(codes.login_wrong_password))
+    //         }
+    //     }, (err) => {
+    //         console.log('error', err)
+    //         return Promise.reject(jsonUtils.getResponseBody(
+    //             codes.other_error, err
+    //         ))
+    //     })
 }
 
 /**
@@ -104,53 +159,59 @@ exports.userLogin = async function (username, password) {
  * @returns {Promise<{code: *, message: *} | {code: *, data: *, message: *}>}
  */
 exports.fetchBaseProfile = async function (userId) {
-    return await repository.getUserById(userId).then((value) => {
-        if (value.length === 0) {
-            return Promise.reject(
-                jsonUtils.getResponseBody(codes.login_wrong_username)
-            )
-        } else {
-            let user = value[0].get()
-            return Promise.resolve(jsonUtils.getResponseBody(codes.success, {
-                id: user.id,
-                username: user.username,
-                nickname: user.nickname,
-                gender: user.gender,
-                avatar: user.avatar,
-                signature: user.signature
-            }))
-        }
-    }, (err) => {
-
-        return Promise.reject(jsonUtils.getResponseBody(codes.other_error, err))
-    })
+    //从数据库中根据id读出该用户
+    let value = null
+    try {
+        value = await repository.getUserById(userId)
+    } catch (e) {
+        return Promise.reject(jsonUtils.getResponseBody(codes.other_error, e))
+    }
+    //如果读出的长度为0，说明用户不存在
+    if (value.length === 0) {
+        return Promise.reject(
+            jsonUtils.getResponseBody(codes.login_wrong_username)
+        )
+    } else {
+        //用户存在，那么将其基本信息打包，返回
+        let user = value[0].get()
+        return Promise.resolve(jsonUtils.getResponseBody(codes.success, {
+            id: user.id,
+            username: user.username,
+            nickname: user.nickname,
+            gender: user.gender,
+            avatar: user.avatar,
+            signature: user.signature
+        }))
+    }
 }
 
 
 /**
  * 根据关键字搜索用户
  * @param text 关键字
- * @returns {Promise<{code: *, data: null, message: *}|{code: *, message: *}>}
  */
 exports.searchUser = async function (text) {
-    return await repository.searchUser(text).then((value) => {
-        let res = []
-        //搜到的用户列表，把关键信息提取出来，打包，返回
-        value.forEach(function (item) {
-            res.push({
-                username: item.username,
-                nickname: item.nickname,
-                avatar: item.avatar,
-                id: item.id,
-                gender: item.gender
-            })
+    //进行数据库搜索
+    let value
+    try {
+        value = await repository.searchUser(text)
+    } catch (e) {
+        return Promise.reject(jsonUtils.getResponseBody(codes.other_error, e))
+    }
+
+    let res = []
+    //搜到的用户列表，把关键信息提取出来，打包，返回
+    value.forEach(function (item) {
+        res.push({
+            username: item.username,
+            nickname: item.nickname,
+            avatar: item.avatar,
+            id: item.id,
+            gender: item.gender
         })
-        console.log("result", res)
-        return Promise.resolve(jsonUtils.getResponseBody(codes.success, res))
-    }, (err) => {
-        console.log("error", err)
-        return Promise.reject(jsonUtils.getResponseBody(codes.other_error, err))
     })
+    console.log("result", res)
+    return Promise.resolve(jsonUtils.getResponseBody(codes.success, res))
 }
 
 
@@ -167,20 +228,22 @@ exports.uploadAvatar = async function (userId, files) {
     await fs.renameSync(files.upload.path, newPath)
     console.log("update_avatar", userId)
     // 通知用户数据库，变更该用户的头像文件名
-    return repository.updateUserAvatar(userId, 'avatar_' + userId + path.extname(files.upload.name)).then((value) => {
-        console.log('va', value)
-        if (value[0]) {
-            //更换头像成功，将头像文件名返回
-            return Promise.resolve(jsonUtils.getResponseBody(codes.success, {
-                file: 'avatar_' + userId + path.extname(files.upload.name)
-            }))
-        } else {
-            // 说明该用户id查找不到任何用户
-            return Promise.reject(jsonUtils.getResponseBody(codes.login_wrong_username))
-        }
-    }, (err) => {
-        return Promise.reject(jsonUtils.getResponseBody(codes.other_error, err))
-    })
+    let value
+    try {
+        value = await repository.updateUserAvatar(userId, 'avatar_' + userId + path.extname(files.upload.name))
+    } catch (e) {
+        return Promise.reject(jsonUtils.getResponseBody(codes.other_error, e))
+    }
+    // 数据库更新成功
+    if (value[0]) {
+        //更换头像成功，将头像文件名返回
+        return Promise.resolve(jsonUtils.getResponseBody(codes.success, {
+            file: 'avatar_' + userId + path.extname(files.upload.name)
+        }))
+    } else {
+        // 说明该用户id查找不到任何用户
+        return Promise.reject(jsonUtils.getResponseBody(codes.login_wrong_username))
+    }
 }
 
 /**
@@ -193,13 +256,14 @@ exports.changeNickname = async function (userId, nickname) {
     let res
     try {
         res = await repository.changeNickname(userId, nickname)
-        if (res[0] > 0) {
-            return Promise.resolve(jsonUtils.getResponseBody(codes.success))
-        } else {
-            return Promise.reject(jsonUtils.getResponseBody(codes.login_wrong_username))
-        }
     } catch (e) {
         return Promise.reject(jsonUtils.getResponseBody(codes.other_error, e))
+    }
+    //判断是否成功变更了某一行
+    if (res[0] > 0) {
+        return Promise.resolve(jsonUtils.getResponseBody(codes.success))
+    } else {
+        return Promise.reject(jsonUtils.getResponseBody(codes.login_wrong_username))
     }
 }
 
@@ -214,13 +278,14 @@ exports.changeSignature = async function (userId, signature) {
     let res
     try {
         res = await repository.changeSignature(userId, signature)
-        if (res[0] > 0) {
-            return Promise.resolve(jsonUtils.getResponseBody(codes.success))
-        } else {
-            return Promise.reject(jsonUtils.getResponseBody(codes.signature_empty))
-        }
     } catch (e) {
         return Promise.reject(jsonUtils.getResponseBody(codes.other_error, e))
+    }
+    //判断是否成功变更了某一行
+    if (res[0] > 0) {
+        return Promise.resolve(jsonUtils.getResponseBody(codes.success))
+    } else {
+        return Promise.reject(jsonUtils.getResponseBody(codes.signature_empty))
     }
 }
 
@@ -229,21 +294,23 @@ exports.changeSignature = async function (userId, signature) {
  * 更改性别
  * @param userId
  * @param gender 性别：MALE/FEMALE
- * @returns {Promise<{code: *, data: null, message: *}|{code: *, message: *}>}
  */
 exports.changeGender = async function (userId, gender) {
+    //输入格式检查
     if (!(tools.inArray(gender, ['MALE', 'FEMALE']))) {
         return Promise.reject(jsonUtils.getResponseBody(codes.format_error_gender))
     }
+    let res
     try {
-        let res = await repository.changeGender(userId, gender)
-        if (res[0] > 0) {
-            return Promise.resolve(jsonUtils.getResponseBody(codes.success))
-        } else {
-            return Promise.reject(jsonUtils.getResponseBody(codes.login_wrong_username))
-        }
+        res = await repository.changeGender(userId, gender)
     } catch (e) {
         return Promise.reject(jsonUtils.getResponseBody(codes.other_error, e))
+    }
+    //判断是否成功变更了某一行
+    if (res[0] > 0) {
+        return Promise.resolve(jsonUtils.getResponseBody(codes.success))
+    } else {
+        return Promise.reject(jsonUtils.getResponseBody(codes.login_wrong_username))
     }
 }
 
@@ -251,7 +318,6 @@ exports.changeGender = async function (userId, gender) {
 /**
  * 根据用户id，查询头像文件名
  * @param userId
- * @returns {Promise<TResult1|undefined>}
  */
 exports.queryAvatar = async function (userId) {
     let path = null
@@ -267,7 +333,7 @@ exports.queryAvatar = async function (userId) {
         return Promise.reject(jsonUtils.getResponseBody(codes.login_wrong_username))
     }
     console.log("path", path)
-    //查询到头像文件民后，调用直接读取函数
+    //查询到头像文件名后，调用直接读取函数
     return this.getAvatar(path)
 }
 
@@ -275,15 +341,14 @@ exports.queryAvatar = async function (userId) {
 /**
  * 根据头像文件名，直接读取头像文件，直接返回给前端
  * @param fileName
- * @returns {Promise<unknown>}
  */
 exports.getAvatar = async function (fileName) {
     try {
         let file = await new Promise((resolve, reject) => {
-            //直接生成头像路径
-            let target = path.join(__dirname, '../') + config.files.avatarDir + '/' + fileName
-            //读取头像文件
-            fs.readFile(target, 'binary', function (err, file) {
+                //直接生成头像路径
+                let target = path.join(__dirname, '../') + config.files.avatarDir + '/' + fileName
+                //读取头像文件
+                fs.readFile(target, 'binary', function (err, file) {
                     if (err) {
                         reject(err)
                     } else if (file === null) {
